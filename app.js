a23a213ecd65e701a451cbcfe81c83766ce6f73c
Akhilanand Singh <akhilanandsingh@gmail.com>
2016-04-16 14:43:56 +0530

    my comment

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var handlebars = require('express-handlebars');
+var bodyparser = require('body-parser');
+var session = require('express-session');
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+
+//****** session
+app.use(session({secret: "secret",  resave : true,  saveUninitialized : false}));
+ 
+
+
+var routes = require('./routes/routes.js');
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars({}));
+
+app.get('/', routes.loginPageHandler);
+app.get('/toLanding', routes.landingPageHandler);
+app.post('/toCity', routes.cityPageHandler);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log('HTTP server is listening on port: ' + port);
+});
